88a13488805303c4a87dc00c8b9cc5015f8b8d17
zehnpaard <zehnpaard@googlemail.com>
2020-05-12 18:27:55 -0700

    Add find for env

--- src/env.ml
+++ src/env.ml
@@ -4,3 +4,9 @@ let empty = []
 
 let extend var val_ env = (var, val_)::env
 let extend_list vvs env = vvs @ env
+
+let rec find var = function
+| [] -> failwith @@ Printf.sprintf "Variable %s not found" var
+| (var', val')::env' ->
+    if var = var' then val'
+    else find var env'

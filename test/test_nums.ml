8d67a0f3d818cdb0502d2851806c188808ccd9cc
zehnpaard <zehnpaard@googlemail.com>
2020-05-13 17:46:16 -0700

    Add tests

---
+++ test/test_nums.ml
@@ -0,0 +1,24 @@
+open OUnit2
+open Kontlang
+
+let test_sum1 _ =
+  let s = "(+ 1)" in
+  assert_equal (Execute.eval_string s) "1"
+
+let test_sum_many _ =
+  let s = "(+ 1 2 3 4 5)" in
+  assert_equal (Execute.eval_string s) "15"
+
+let test_sub_many _ =
+  let s = "(- 15 1 2 3 4 5)" in
+  assert_equal (Execute.eval_string s) "0"
+
+let suite =
+  "ExampleTestList" >::: [
+    "test_sum1" >:: test_sum1
+  ; "test_sum_many" >:: test_sum_many
+  ; "test_sub_many" >:: test_sub_many
+  ]
+
+let () =
+  run_test_tt_main suite

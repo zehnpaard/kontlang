1812a0b29ea73be5ef152e8b31ce08e025eaeb19
zehnpaard <zehnpaard@googlemail.com>
2020-05-13 17:34:47 -0700

    Move functionality from main to execute

--- main.ml
+++ main.ml
@@ -1,15 +1,8 @@
 open Kontlang
 
-let f s =
-  Lexing.from_string s
-  |> Parser.f Lexer.f
-  |> Execute.eval (Builtins.load Env.empty)
-  |> Val.to_string
-  |> print_endline
-
 let rec read_eval_print exp_string =
   let s = read_line () in
-  if s = "" then f exp_string
+  if s = "" then Execute.eval_string exp_string |> print_endline
   else read_eval_print (Printf.sprintf "%s\n%s" exp_string s)
 
 let () = (print_string ">>> "; read_eval_print "")
--- src/execute.ml
+++ src/execute.ml
@@ -6,3 +6,9 @@ let rec eval env = function
   | v ->
       let s = Val.to_string v in
       failwith @@ Printf.sprintf "Non-function %s in operator position" s)
+
+let eval_string s =
+  Lexing.from_string s
+  |> Parser.f Lexer.f
+  |> eval (Builtins.load Env.empty)
+  |> Val.to_string

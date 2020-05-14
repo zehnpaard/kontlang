07dd8f4e395161054962fada40ed3ee369e8c039
zehnpaard <zehnpaard@googlemail.com>
2020-05-14 16:29:07 -0700

    Add apply_cont to execute

--- src/execute.ml
+++ src/execute.ml
@@ -1,14 +1,16 @@
-let rec eval env = function
-| Exp.Int n -> Val.Int n
-| Exp.Var s -> Env.find s env
-| Exp.Call (e, es) -> (match eval env e with
-  | Val.Op (_, fn) -> fn @@ List.map (eval env) es
-  | v ->
-      let s = Val.to_string v in
-      failwith @@ Printf.sprintf "Non-function %s in operator position" s)
+let rec eval env cont = function
+| Exp.Int n -> apply_cont env cont @@ Val.Int n
+| Exp.Var s -> apply_cont env cont @@ Env.find s env
+| Exp.Call (e, es) -> eval env (Cont.Call (es, []) :: cont) e
+and apply_cont env cont v = match cont with
+| [] -> v
+| Cont.Call ([], vs) :: cont' -> (match List.rev (v::vs) with
+  | Val.Op (_, fn) :: vs' -> apply_cont env cont' (fn vs')
+  | _ -> failwith "Calling non-op in operator position")
+| Cont.Call (e::es, vs) :: cont' -> eval env (Cont.Call (es, v::vs) :: cont') e
 
 let eval_string s =
   Lexing.from_string s
   |> Parser.f Lexer.f
-  |> eval (Builtins.load Env.empty)
+  |> eval (Builtins.load Env.empty) Cont.final
   |> Val.to_string

b62765b906d1c979a0f17ab0ca226430821d871a
zehnpaard <zehnpaard@googlemail.com>
2020-05-16 19:31:45 -0700

    Add let exp

--- src/exp.ml
+++ src/exp.ml
@@ -3,6 +3,7 @@ type t =
 | Var of string
 | Call of t * t list
 | If of t * t * t
+| Let of string * t * t
 
 let rec to_string = function
 | Int n -> string_of_int n
@@ -17,3 +18,7 @@ let rec to_string = function
     let s2 = (to_string e2) in
     let s3 = (to_string e3) in
     Printf.sprintf "(if %s %s %s)" s1 s2 s3
+| Let (v1, e1, e2) ->
+    let s1 = to_string e1 in
+    let s2 = to_string e2 in
+    Printf.sprintf "(let %s %s %s)" v1 s1 s2

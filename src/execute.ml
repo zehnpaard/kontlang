c998bc396ba44e3d395709ca6a14bf11aa5b2bb4
zehnpaard <zehnpaard@googlemail.com>
2020-05-16 19:39:41 -0700

    Add let clauses for eval and apply_cont

--- src/execute.ml
+++ src/execute.ml
@@ -8,6 +8,7 @@ let eval env cont = function
 | Exp.Var s -> ApplyCont(env, cont, Env.find s env)
 | Exp.Call(e, es) -> Eval(env, Cont.Call(es, []) :: cont, e)
 | Exp.If(e1, e2, e3) -> Eval(env, Cont.If(e2, e3) :: cont, e1)
+| Exp.Let(s, e1, e2) -> Eval(env, Cont.Let(s, e2) :: cont, e1)
 
 let apply_cont env cont v = match cont with
 | [] -> Done v
@@ -18,6 +19,7 @@ let apply_cont env cont v = match cont with
 | Cont.If(e2, e3) :: cont' -> (match v with
   | Val.Bool b -> Eval(env, cont', if b then e2 else e3)
   | _ -> failwith "Non-boolean in condition position of If expression")
+| Cont.Let(s, e2) :: cont' -> Eval(Env.extend s v env, cont', e2)
 
 let rec trampoline = function
 | Done v -> v

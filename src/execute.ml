d1721a07bef45de498f79757c39c459d92d3340f
zehnpaard <zehnpaard@googlemail.com>
2020-05-17 18:18:26 -0700

    Add multi-var let to continuation and evaluation

--- src/execute.ml
+++ src/execute.ml
@@ -8,7 +8,8 @@ let eval env cont = function
 | Exp.Var s -> ApplyCont(env, cont, Env.find s env)
 | Exp.Call(e, es) -> Eval(env, Cont.Call(es, []) :: cont, e)
 | Exp.If(e1, e2, e3) -> Eval(env, Cont.If(e2, e3) :: cont, e1)
-| Exp.Let(s, e1, e2) -> Eval(env, Cont.Let(s, e2) :: cont, e1)
+| Exp.Let((s, e1)::ves, e2) -> Eval(env, Cont.Let(s, ves, [], e2) :: cont, e1)
+| Exp.Let _ -> failwith "Evaluating empty Let"
 
 let apply_cont env cont v = match cont with
 | [] -> Done v
@@ -19,7 +20,11 @@ let apply_cont env cont v = match cont with
 | Cont.If(e2, e3) :: cont' -> (match v with
   | Val.Bool b -> Eval(env, cont', if b then e2 else e3)
   | _ -> failwith "Non-boolean in condition position of If expression")
-| Cont.Let(s, e2) :: cont' -> Eval(Env.extend s v env, cont', e2)
+| Cont.Let(s, [], vvs, e2) :: cont' ->
+    let env' = Env.extend_list (List.rev ((s, v)::vvs)) env in
+    Eval(env', cont', e2)
+| Cont.Let(s, (s', e')::ves, vvs, e2) :: cont' ->
+    Eval(env, Cont.Let(s', ves, (s, v)::vvs, e2) :: cont', e')
 
 let rec trampoline = function
 | Done v -> v

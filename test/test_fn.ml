82f77bd4f549a6f23cdf9365a48454a21c8e37da
zehnpaard <zehnpaard@googlemail.com>
2020-05-20 03:52:52 -0700

    Add let_fn and fn_let tests

--- test/test_fn.ml
+++ test/test_fn.ml
@@ -11,10 +11,20 @@ let test_fn_error1 _ =
   let msg = "Function anon called with incorrect number of args: expected 1 received 2" in
   assert_raises (Failure msg) thunk
 
+let test_let_fn _ =
+  let s = "(let [x 3] ((fn [x] (+ x x)) x))" in
+  assert_equal (Execute.eval_string s) "6"
+
+let test_fn_let _ =
+  let s = "((fn [x] (+ x (let [x 3] x))) 5)" in
+  assert_equal (Execute.eval_string s) "8"
+
 let suite =
   "FnTestList" >::: [
     "test_fn1" >:: test_fn1
   ; "test_fn_error1" >:: test_fn_error1
+  ; "test_let_fn" >:: test_let_fn
+  ; "test_fn_let" >:: test_fn_let
   ]
 
 let () =

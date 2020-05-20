fcab318fe613e5e62255894ecb2b2ac9641f4454
zehnpaard <zehnpaard@googlemail.com>
2020-05-19 19:07:37 -0700

    Add Fn val

--- src/val.ml
+++ src/val.ml
@@ -2,8 +2,10 @@ type t =
 | Int of int
 | Bool of bool
 | Op of string * (t list -> t)
+| Fn of string * string list * Exp.t
 
 let to_string = function
 | Int n -> string_of_int n
 | Bool b -> string_of_bool b
 | Op (s, _) -> Printf.sprintf "Op(%s)" s
+| Fn (s, _, _) -> Printf.sprintf "Fn(%s)" s

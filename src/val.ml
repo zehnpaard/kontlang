264ea7c29b535230f53dcda0d6e6b752cee03b4e
zehnpaard <zehnpaard@googlemail.com>
2020-05-23 16:06:07 -0700

    Add RecFn val

--- src/val.ml
+++ src/val.ml
@@ -3,9 +3,11 @@ type t =
 | Bool of bool
 | Op of string * (t list -> t)
 | Fn of string * string list * (string * t) list * Exp.t
+| RecFn of string * string list * (string * t) list ref * Exp.t
 
 let to_string = function
 | Int n -> string_of_int n
 | Bool b -> string_of_bool b
 | Op (s, _) -> Printf.sprintf "Op(%s)" s
 | Fn (s, _, _, _) -> Printf.sprintf "Fn(%s)" s
+| RecFn (s, _, _, _) -> Printf.sprintf "FnRec(%s)" s

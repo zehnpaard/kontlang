c10c047a643f4f2068643016108da90bfb7d7391
zehnpaard <zehnpaard@googlemail.com>
2020-05-24 08:49:47 -0700

    Add Cons to val

--- src/val.ml
+++ src/val.ml
@@ -4,10 +4,12 @@ type t =
 | Op of string * (t list -> t)
 | Fn of string * string list * (string * t) list * Exp.t
 | RecFn of string * string list * (string * t) list ref * Exp.t
+| Cons of t * t
 
-let to_string = function
+let rec to_string = function
 | Int n -> string_of_int n
 | Bool b -> string_of_bool b
 | Op(s, _) -> Printf.sprintf "Op(%s)" s
 | Fn(s, _, _, _) -> Printf.sprintf "Fn(%s)" s
 | RecFn(s, _, _, _) -> Printf.sprintf "FnRec(%s)" s
+| Cons(x, y) -> Printf.sprintf "(%s, %s)" (to_string x) (to_string y)

490e152ab1cf778be6abdc03980cdaaa053b8cec
zehnpaard <zehnpaard@googlemail.com>
2020-05-25 10:39:29 -0700

    Add tests for do

--- test/test_side_effect.ml
+++ test/test_side_effect.ml
@@ -13,11 +13,21 @@ let test_concat2 _ =
   let s = "(let [x \"c\"] (concat \"a\" \"b\" x))" in
   assert_equal (Execute.eval_string s) "\"abc\""
 
+let test_do1 _ =
+  let s = "(do [1])" in
+  assert_equal (Execute.eval_string s) "1"
+
+let test_do2 _ =
+  let s = "(do [1 2 3])" in
+  assert_equal (Execute.eval_string s) "3"
+
 let suite =
   "SideEffectTestList" >::: [
     "test_string1" >:: test_string1
   ; "test_concat1" >:: test_concat1
   ; "test_concat2" >:: test_concat2
+  ; "test_do1" >:: test_do1
+  ; "test_do2" >:: test_do2
   ]
 
 let () =

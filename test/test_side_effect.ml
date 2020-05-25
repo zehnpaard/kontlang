63ebe1bc05d43afcac96056aa9cb788c6d74c7ff
zehnpaard <zehnpaard@googlemail.com>
2020-05-25 10:20:05 -0700

    Add tests for string/concat

---
+++ test/test_side_effect.ml
@@ -0,0 +1,24 @@
+open OUnit2
+open Kontlang
+
+let test_string1 _ =
+  let s = "\"hello world\"" in
+  assert_equal (Execute.eval_string s) "\"hello world\""
+
+let test_concat1 _ =
+  let s = "(concat \"a\" \"b\")" in
+  assert_equal (Execute.eval_string s) "\"ab\""
+
+let test_concat2 _ =
+  let s = "(let [x \"c\"] (concat \"a\" \"b\" x))" in
+  assert_equal (Execute.eval_string s) "\"abc\""
+
+let suite =
+  "SideEffectTestList" >::: [
+    "test_string1" >:: test_string1
+  ; "test_concat1" >:: test_concat1
+  ; "test_concat2" >:: test_concat2
+  ]
+
+let () =
+  run_test_tt_main suite

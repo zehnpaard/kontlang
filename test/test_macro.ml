db3ccb10b8dbc7ddc376a63dfe98810097b154f8
zehnpaard <zehnpaard@googlemail.com>
2020-05-25 14:13:09 -0700

    Add tests for macros

---
+++ test/test_macro.ml
@@ -0,0 +1,20 @@
+open OUnit2
+open Kontlang
+
+let test_macro1 _ =
+  let s = "((macro [x] nil) (print (read)))" in
+  assert_equal (Execute.eval_string s) "nil"
+
+let test_macro_dynamic1 _ =
+  let s = "(let [m (macro [x] (+ x y))]
+             (let [y 10] (m 5)))" in
+  assert_equal (Execute.eval_string s) "15"
+
+let suite =
+  "MacroTestList" >::: [
+    "test_macro1" >:: test_macro1
+  ; "test_macro_dynamic1" >:: test_macro_dynamic1
+  ]
+
+let () =
+  run_test_tt_main suite

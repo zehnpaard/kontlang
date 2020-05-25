98e79d3f1b1ce3ad820b8f97717150cd3fb25936
zehnpaard <zehnpaard@googlemail.com>
2020-05-25 10:39:22 -0700

    Update Cont and Execute for do

--- src/execute.ml
+++ src/execute.ml
@@ -38,6 +38,8 @@ let eval env cont = function
     let fname_fns, fvalsrs = List.split @@ List.map f fns in
     List.iter (fun fvalsr -> (fvalsr := (fname_fns @ !fvalsr))) fvalsrs;
     Eval(Env.extend_list fname_fns env, Cont.Env::cont, e)
+  | Exp.Do(e::es) -> Eval(env, Cont.Do es::cont, e)
+  | Exp.Do([]) -> failwith "Evaluating empty do"
 
 let apply_cont env cont v = match cont with
 | [] -> Done v
@@ -67,6 +69,8 @@ let apply_cont env cont v = match cont with
     Eval(Env.extend_current s v env, cont', e2)
 | Cont.Lets(s, (s', e')::ves, e2) :: cont' ->
     Eval(Env.extend_current s v env, Cont.Lets(s', ves, e2) :: cont', e')
+| Cont.Do(e::es)::cont' -> Eval(env, Cont.Do es::cont', e)
+| Cont.Do([])::cont' -> ApplyCont(env, cont', v)
 | Cont.Env :: cont' -> ApplyCont (Env.pop env, cont', v)
 
 let rec trampoline = function

3d542bf31e01c0b257e11f76f876fc78ade33145
zehnpaard <zehnpaard@googlemail.com>
2020-06-20 10:15:11 -0700

    Add more tests for include

--- test/test_include.ml
+++ test/test_include.ml
@@ -16,10 +16,34 @@ let test_include2 _ =
              (+ M.x N.y))" in
   assert_equal (Execute.eval_string s) "25"
 
+let test_include3 _ =
+  let s = "(let* [(M (module [(define x 10)]))
+                  (N (module [(define y (+ x 5))
+                              (include M)]))]
+             (+ M.x N.y))" in
+  assert_raises (Failure "Variable x not found") (fun () -> Execute.eval_string s)
+
+let test_multi_include1 _ =
+  let s = "(let* [(M (module [(define x 10)]))
+                  (N (module [(define y 5)]))
+                  (O (module [(include M) (include N)]))]
+             (+ O.x O.y))" in
+  assert_equal (Execute.eval_string s) "15"
+
+let test_multi_include2 _ =
+  let s = "(let* [(M (module [(define x 10)]))
+                  (N (module [(define y 5)]))
+                  (O (module [(include M) (include N) (define z (+ x y))]))]
+             (+ O.x O.y O.z))" in
+  assert_equal (Execute.eval_string s) "30"
+
 let suite =
   "IncludeTestList" >::: [
     "test_include1" >:: test_include1
   ; "test_include2" >:: test_include2
+  ; "test_include3" >:: test_include3
+  ; "test_multi_include1" >:: test_multi_include1
+  ; "test_multi_include2" >:: test_multi_include2
   ]
 
 let () =

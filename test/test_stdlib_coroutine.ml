a222eb23213f4f491d52331e72296f51ab43941b
zehnpaard <zehnpaard@googlemail.com>
2020-06-26 19:01:03 -0700

    Add helper tests for coroutines

--- test/test_stdlib_coroutine.ml
+++ test/test_stdlib_coroutine.ml
@@ -69,6 +69,72 @@ let test_rec_cr1 _ =
   " in
   assert_equal (Execute.eval_string s) "6"
 
+let test_cr_helper1 _ =
+  let s = "
+  (let* [(C Stdlib.Coroutine)
+         (c (C.coroutine (let [x (C.yield 1)] x)))]
+    (C.val (C.start c)))
+  " in
+  assert_equal (Execute.eval_string s) "1"
+
+let test_cr_helper2 _ =
+  let s = "
+  (let* [(C Stdlib.Coroutine)
+         (c (C.coroutine (let [x (C.yield 1)] x)))
+         (c (C.start c))]
+    (C.val (C.send c 2)))
+  " in
+  assert_equal (Execute.eval_string s) "2"
+
+let test_cr_helper3 _ =
+  let s = "
+  (let* [(C Stdlib.Coroutine)
+         (c (C.coroutine (let [x (C.yield 1)] x)))
+         (c (C.start c))
+         (c (C.send c 2))]
+    (C.send c 3))
+  " in
+  assert_equal (Execute.eval_string s) "nil"
+
+let test_multi_cr_helper1 _ =
+  let s = "
+  (let* [(C Stdlib.Coroutine)
+         (c (C.coroutine
+              (let [(x (C.yield 1))
+                    (y (C.yield 2))]
+                (+ x y))))
+         (c (C.start c))
+         (c (C.send c 3))]
+    (C.val (C.send c 4)))
+  " in
+  assert_equal (Execute.eval_string s) "7"
+
+let test_multi_cr_helper2 _ =
+  let s = "
+  (let* [(C Stdlib.Coroutine)
+         (c (C.coroutine
+              (let [(x (C.yield 1))
+                    (y (C.yield 2))]
+                (+ x y))))
+         (c (C.start c))
+         (c (C.send c 3))
+         (c (C.send c 4))]
+    (C.send c 5))
+  " in
+  assert_equal (Execute.eval_string s) "nil"
+
+let test_rec_cr_helper1 _ =
+  let s = "
+  (let [(C Stdlib.Coroutine)]
+    (letrec [f [x] (f (+ x (C.yield x)))]
+      (let* [(c (C.coroutine (f 0)))
+             (c (C.start c))
+             (c (C.send c 1))
+             (c (C.send c 2))]
+        (C.val (C.send c 3)))))
+  " in
+  assert_equal (Execute.eval_string s) "6"
+
 let suite =
   "CoroutineTestList" >::: [
     "test_cr1" >:: test_cr1
@@ -77,6 +143,12 @@ let suite =
   ; "test_multi_cr1" >:: test_multi_cr1
   ; "test_multi_cr2" >:: test_multi_cr2
   ; "test_rec_cr1" >:: test_rec_cr1
+  ; "test_cr1" >:: test_cr_helper1
+  ; "test_cr2" >:: test_cr_helper2
+  ; "test_cr3" >:: test_cr_helper3
+  ; "test_multi_cr1" >:: test_multi_cr_helper1
+  ; "test_multi_cr2" >:: test_multi_cr_helper2
+  ; "test_rec_cr1" >:: test_rec_cr_helper1
   ]
 
 let () =
